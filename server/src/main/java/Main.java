1255984860ee83d21698c53b87ae5207c6293541
Parker Reyes <reyespar@byu.edu>
2025-06-11 15:44:47 -0600

    Implement additional parts of Database manager

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -3,12 +3,12 @@ import dataaccess.DataAccessException;
 import server.Server;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DataAccessException {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
 
-
         Server server = new Server();
         int port = server.run(8080);
+        System.out.println("Listening on port " + port);
     }
 }
