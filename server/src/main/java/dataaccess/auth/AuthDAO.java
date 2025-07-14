1411f2a1f27efdd751e2e26721c43dd77c3ef69d
Parker Reyes <reyespar@byu.edu>
2025-07-13 23:44:38 -0600

    Start AuthDAO and memoryAuthDAO

--- server/src/main/java/dataaccess/auth/AuthDAO.java
+++ server/src/main/java/dataaccess/auth/AuthDAO.java
@@ -1,4 +1,10 @@
 package dataaccess.auth;
 
-public class AuthDAO {
+import model.AuthData;
+import dataaccess.DataAccessException;
+
+public interface AuthDAO {
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/auth/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/auth/MemoryAuthDAO.java
@@ -1,4 +1,27 @@
 package dataaccess.auth;
 
-public class MemoryAuthDAO {
+import model.AuthData;
+import dataaccess.DataAccessException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryAuthDAO implements AuthDAO {
+    private final Map<String,AuthData> store = new ConcurrentHashMap<>();
+
+    @Override
+    public void createAuth(AuthData auth) {
+        store.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String token) throws DataAccessException {
+        AuthData a = store.get(token);
+        if (a == null) throw new DataAccessException("AuthToken not found");
+        return a;
+    }
+
+    @Override
+    public void deleteAuth(String token) {
+        store.remove(token);
+    }
 }
