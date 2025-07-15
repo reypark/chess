05018f0aea3ed19d9ec8f317276145297ad6a668
Parker Reyes <reyespar@byu.edu>
2025-07-14 20:25:33 -0600

    Start UserDAO and MemoryUserDAO

--- server/src/main/java/dataaccess/user/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/user/MemoryUserDAO.java
@@ -1,4 +1,35 @@
 package dataaccess.user;
 
-public class MemoryUserDAO {
+import dataaccess.DataAccessException;
+import model.UserData;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryUserDAO implements UserDAO {
+    private final Map<String, UserData> store = new ConcurrentHashMap<>();
+
+    @Override
+    public void clear() {
+        store.clear();
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (user == null || user.username() == null || user.username().isEmpty()) {
+            throw new DataAccessException("Invalid user data");
+        }
+        if (store.containsKey(user.username())) {
+            throw new DataAccessException("User already exists: " + user.username());
+        }
+        store.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        if (username == null || !store.containsKey(username)) {
+            throw new DataAccessException("User not found: " + username);
+        }
+        return store.get(username);
+    }
 }
--- server/src/main/java/dataaccess/user/UserDAO.java
+++ server/src/main/java/dataaccess/user/UserDAO.java
@@ -1,4 +1,10 @@
 package dataaccess.user;
 
-public class UserDAO {
+import dataaccess.DataAccessException;
+import model.UserData;
+
+public interface UserDAO {
+    void clear() throws DataAccessException;
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
 }
