fe7475a1175190db545ff8f20d3ffb329a679e35
Parker Reyes <reyespar@byu.edu>
2025-07-27 21:25:45 -0600

    Implement beginning of REPL with help menu and quit

---
+++ client/src/main/java/Repl.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Repl {
+    private enum State {LOGGED_OUT}
+    private State state = State.LOGGED_OUT;
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        printWelcome();
+
+        while (running) {
+            System.out.print("[" + state + "] >>> ");
+            String line = scanner.nextLine().trim().toLowerCase();
+
+            switch (line) {
+                case "help":
+                    printHelp();
+                    break;
+                case "quit":
+                    running = false;
+                    break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private void printWelcome() {
+        System.out.println("Welcome to 240 Chess. Type Help to get started.");
+    }
+
+    private void printHelp() {
+        System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
+        System.out.println("login <USERNAME> <PASSWORD> - to play chess");
+        System.out.println("quit - playing chess");
+        System.out.println("help - with possible commands");
+    }
+}
